8768258e7a0589f9733762258c7707cc9b3a041b
X-Liang <1129584401@qq.com>
2017-11-18 18:25:02 +0800

    RAC 使用联系，RACUsingViewController

--- ReactiveCocoa/ReactiveCocoa/ReactiveCocoa/ReactiveCocoa/RACUsingViewController.m
+++ ReactiveCocoa/ReactiveCocoa/ReactiveCocoa/ReactiveCocoa/RACUsingViewController.m
@@ -19,7 +19,10 @@
  6. 处理一个界面，多个请求时的问题
  */
 @interface RACUsingViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIButton *button;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (nonatomic, assign) int age;
 @end
 
 @implementation RACUsingViewController
@@ -37,8 +40,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+//    [self replaceKVO];
+//    [self listenAction];
+//    [self listenNotify];
+//    [self listenTextField];
+    [self loadData];
 }
-
+ #pragma mark - 监听某个方法有没有调用
 - (void)signalForSelector {
     CustomView *view = [[CustomView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
     view.backgroundColor = [UIColor redColor];
@@ -48,6 +56,92 @@
     [[view rac_signalForSelector:@selector(touchesBegan:withEvent:)] subscribeNext:^(id _Nullable x) {
         NSLog(@"%@",x);
     }];
+}
+
+- (void)dealloc {
+    [self removeObserver:self forKeyPath:@"age"];
+}
+#pragma mark - KVO
+- (void)replaceKVO {
+    // keypath 用来获取某个对象的属性并转换为字符串
+    // keypath(self, age) 相当于 path
+    [[self rac_valuesAndChangesForKeyPath:@keypath(self,age)
+                                  options:NSKeyValueObservingOptionNew
+                                 observer:self] subscribeNext:^(RACTwoTuple<id,NSDictionary *> * _Nullable x) {
+        NSLog(@"%@",x);
+    }] ;
+    
+    // 通过宏来实现 KVO
+    [RACObserve(self, age) subscribeNext:^(id  _Nullable x) {
+        NSLog(@"%@",x);
+    }];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    self.age++;
+    [self postNotify];
+}
+#pragma mark - 监听事件
+- (void)listenAction {
+    [[self.button rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(__kindof UIControl * _Nullable x) {
+        NSLog(@"点击了按钮%@",x);
+    }];
+}
+#pragma mark - 通知
+- (void)listenNotify {
+    // 监听通知
+    // object 表示谁发出的通知
+    //rac 通知中不需要管理通知观察者
+    [[[NSNotificationCenter defaultCenter] rac_addObserverForName:@"Notify" object:self] subscribeNext:^(NSNotification * _Nullable x) {
+        NSLog(@"监听到通知%@",x);
+    }];
+}
+
+- (void)postNotify {
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"Notify" object:self];
+}
+
+#pragma mark - 绑定信号
+- (void)listenTextField {
+//    [[self.textField rac_textSignal] subscribeNext:^(NSString * _Nullable x) {
+//        NSLog(@"%@",x);
+//        self.label.text = x;
+//    }];
+    
+    // 给 label 的 text 属性进行绑定
+    // 将属性的值与信号进行保定
+    RAC(self.label, text) = [self.textField rac_textSignal];
+}
+
+#pragma mark - 处理一个界面，多个请求时的问题
+- (void)loadData {
+    // 当数组中所有信号都发送一次next 时候，才会执行 lift selector
+    // 右边 array 中有几个信号，指定的 selector 必须有几个参数
+    [self rac_liftSelector:@selector(updateUIHotData:newData:)   withSignalsFromArray:@[[self loadHotData], [self loadNewData]]];
+}
+
+// 请求最热板块数据
+- (RACSignal *)loadHotData {
+    return [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [subscriber sendNext:@[@{@"1":@"one", @"2":@"two" }]];
+        });
+        return nil;
+    }];
+    
+}
+// 请求上新板块数据
+- (RACSignal *)loadNewData {
+    return [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [subscriber sendNext:@"最新板块数据"];
+        });
+        return nil;
+    }];
+}
+
+- (void)updateUIHotData: (NSArray *)hot newData: (NSString *)new {
+    NSLog(@"update ui hot: %@ new:%@", hot, new);
 }
 
 @end
